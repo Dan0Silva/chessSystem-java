2b0468ab8452975efc863dbdf33df51b788a1879
Danilo Araujo <danilo.official22@hotmail.com>
2020-08-18 12:07:58 -0300

    Firsh class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main (String[] args) {
 		
-		System.out.println("Hello Word!");
-		
+		Position ps = new Position(3,5);
+		System.out.println(ps);
 	}
 	
 }

04bdd037c46a8b408fd87c313d8e30fac930c54b
Danilo Araujo <danilo.official22@hotmail.com>
2020-08-28 13:13:40 -0300

    BoardExeption and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,36 +5,59 @@ public class Board {
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
-	
+
 	public Board(int rows, int columns) {
-		this.rows = rows;
-		this.columns = columns;
-		pieces = new Piece[rows][columns];
+		if (rows < 1 || columns < 0) {
+			throw new BoardExeption("Erro criando tabulerio: É nescessario que haja pelo menos uma linha e uma coluna.");
+		} else {
+			this.rows = rows;
+			this.columns = columns;
+			pieces = new Piece[rows][columns];
+		}
 	}
-	
+
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+
 	public int getColumns() {
 		return columns;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
+
 	public Piece piece(int row, int column) {
+		if (!positionExists(row,column)) {
+			throw new BoardExeption("Posição fora do tabuleiro.");
+		}
 		return pieces[row][column];
 	}
-	
+
 	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardExeption("Posição fora do tabuleiro.");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardExeption("Posição "+position+" já ocupada.");
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardExeption("Posição fora do tabuleiro.");
+		}
+		return piece(position) != null;
 	}
 }

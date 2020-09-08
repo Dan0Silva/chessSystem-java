6076319de9c9a4362091ea528b872a17dae6f3a4
Danilo Araujo <danilo.official22@hotmail.com>
2020-09-08 12:51:42 -0300

    Little improvement in board printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -5,6 +5,8 @@ import chess.Color;
 
 public class UI {
 
+	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -25,27 +27,29 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
 	public static void printBoard(ChessPiece[][] pieces) {
-		for (int i=0;i<pieces.length;i++) {
-			System.out.print((8-i)+" ");
-			for(int j=0;j<pieces.length;j++) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h ");
+		System.out.println("  a b c d e f g h");
 	}
-	
+		
 	private static void printPiece(ChessPiece piece) {
-		if (piece == null) {
-			System.out.print("-");
-		}else {
-			if(piece.getColor() == Color.WHITE){
-				System.out.println(ANSI_WHITE + piece + ANSI_RESET);
-			}else {
-				System.out.println(ANSI_RED + piece + ANSI_RESET);
-			}
-		}
 		System.out.print(" ");
+    	if (piece == null) {
+            System.out.print("-");
+        }
+        else {
+            if (piece.getColor() == Color.WHITE) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
+        }
+        System.out.print(" ");
 	}
-	
 }

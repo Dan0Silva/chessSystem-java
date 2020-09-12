35c0b6d5e59718bb9d0b85b5c4961d5315e0df2c
Danilo Araujo <danilo.official22@hotmail.com>
2020-09-12 19:34:43 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -43,7 +43,10 @@ public class ChessMatch {
 	
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) {
-			throw new ChessException("Não existe peça na posição.");
+			throw new ChessException("Nao existe peca na posicao.");
+		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Nao existe movimentos possiveis para a peca escolhida.");
 		}
 	}
 	

5a5632a2a82b4e7c05f11a7a76564359518b0418
Danilo Araujo <danilo.official22@hotmail.com>
2020-09-28 17:12:45 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,12 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(cm.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					cm.replacePromotedPiece(type);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
